ba7b56a607a26f0471f728bb793639fa644c3fd5
Preethi <preethi@distill.io>
2021-08-05 23:55:49 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Buttons from "./Buttons.svelte";
+</script>
+
+<main>
+	<Buttons />
+</main>
+
+<style>
+</style>
---
+++ src/Buttons.svelte
@@ -0,0 +1,149 @@
+<script>
+    let clicked = "";
+    let total = "";
+    function onNumberClick(number) {
+        debugger;
+        if (number === "=") {
+            total = eval(clicked);
+        } else {
+            clicked = clicked + number;
+        }
+    }
+    function onClear() {
+        clicked = "";
+        total = "";
+    }
+    function onDelete() {
+        clicked = clicked.slice(0, -1);
+    }
+</script>
+
+<div class="h-auto w-80 flex flex-col border-2 border-gray-300 bg-gray-300">
+    <input
+        bind:value={clicked}
+        class="flex-auto h-10 w-full border-2 bg-white"
+    />
+    <input bind:value={total} class="flex-auto h-10 w-full border-2 bg-white" />
+    <div class="flex-auto h-auto w-full flex flex-row">
+        <button
+            on:click={onClear}
+            class="flex flex-grow m-1 p-1 justify-center items-center bg-white"
+            >Clear</button
+        >
+        <button
+            on:click={onDelete}
+            class="flex flex-grow m-1 p-1 justify-center items-center bg-white"
+            >Delete</button
+        >
+    </div>
+    <div class="flex-auto">
+        <div class="flex flex-col">
+            <div class="flex flex-row justify-center items-center">
+                <div
+                    on:click={() => onNumberClick("7")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    7
+                </div>
+                <div
+                    on:click={() => onNumberClick("8")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    8
+                </div>
+                <div
+                    on:click={() => onNumberClick("9")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    9
+                </div>
+                <div
+                    on:click={() => onNumberClick("/")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    /
+                </div>
+            </div>
+            <div class="flex flex-row justify-center items-center">
+                <div
+                    on:click={() => onNumberClick("4")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    4
+                </div>
+                <div
+                    on:click={() => onNumberClick("5")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    5
+                </div>
+                <div
+                    on:click={() => onNumberClick("6")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    6
+                </div>
+                <div
+                    on:click={() => onNumberClick("-")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    -
+                </div>
+            </div>
+            <div class="flex flex-row justify-center items-center">
+                <div
+                    on:click={() => onNumberClick("1")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    1
+                </div>
+                <div
+                    on:click={() => onNumberClick("2")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    2
+                </div>
+                <div
+                    on:click={() => onNumberClick("3")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    3
+                </div>
+                <div
+                    on:click={() => onNumberClick("+")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    +
+                </div>
+            </div>
+            <div class="flex flex-row justify-center items-center">
+                <div
+                    on:click={() => onNumberClick("0")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    0
+                </div>
+                <div
+                    on:click={() => onNumberClick(".")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    .
+                </div>
+                <div
+                    on:click={() => {
+                        onNumberClick("=");
+                    }}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    =
+                </div>
+                <div
+                    on:click={() => onNumberClick("x")}
+                    class="flex-auto m-2 p-2 bg-white text-center"
+                >
+                    x
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
